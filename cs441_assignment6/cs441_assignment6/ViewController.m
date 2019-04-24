4f3c539c1c541c71626a120dc171eb8036636806
Kevin Wu <kevinwu@Kevins-MacBook-Air-4.local>
2019-04-24 13:59:56 -0400

    toggle

--- cs441_assignment6/cs441_assignment6/ViewController.m
+++ cs441_assignment6/cs441_assignment6/ViewController.m
@@ -18,6 +18,17 @@
 @synthesize image;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if(toggle == 0){
+        [label setText:@"I love Java and Android!"];
+        image.image = [UIImage imageNamed : @"madden.png"];
+        toggle = 1;
+    }
+    else
+    {
+        [label setText:@ "Hello World"];
+        image.image = [UIImage imageNamed : @"iOS_11_GM_Wallpaper_Earth.jpg"];
+        toggle = 0;
+    }
     // Do any additional setup after loading the view.
 }
 for (int i = 0; i < 100; i ++){
